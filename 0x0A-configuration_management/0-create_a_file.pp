55145399714c8201d677423466d473c6c3bd100d
DNeosphere <daniela.gomezp05@outlook.com>
2019-12-09 22:32:49 +0000

    Fix: properties order

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Creates a file
 file { '/etc':
 path    => '/tmp/holberton',
-content => 'I Love Puppet',
 mode    => '0744',
 owner   => 'www-data',
 group   => 'www-data',
+content => 'I love Puppet'
 }
