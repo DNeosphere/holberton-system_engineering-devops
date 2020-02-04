4a417e9db21847da9877f2051f6f52f83d183baf
dneosphere <daniela.gomezp05@outlook.com>
2020-02-04 01:33:05 +0000

    Feature: task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Write a Python script that, using this REST API,
+for a given employee ID, returns information about
+his/her TODO list progress.
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    user_id = int(argv[1])
+    users = requests.get("https://jsonplaceholder.typicode.com/users").json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+
+    """ get user """
+    for user in users:
+        if user.get('id') == user_id:
+            n_user = user
+
+    """ get todos """
+    task_list = []
+    for task in todos:
+        if task.get('userId') == user_id:
+            task_list.append(task)
+
+
+    """ finde completed todos """
+    completed_tasks = []
+    for task in task_list:
+        if task.get('completed') is True:
+            completed_tasks.append(task)
+
+    """ print with given format """
+    print("Employee {} is done with tasks({}/{}):".format(n_user.get('name'), \
+                                                          len(completed_tasks), \
+                                                          len(task_list)))
+
+    for print_task in completed_tasks:
+        print("\t {}".format(print_task.get('title')))
