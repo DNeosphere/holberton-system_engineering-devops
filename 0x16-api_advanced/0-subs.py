94de18c85ab948780524b73568bc245ab54f77c8
dneosphere <daniela.gomezp05@outlook.com>
2020-02-06 17:54:56 +0000

    Feature: task 0, subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns the number of subscribers
+"""
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ get request obtain suscribers """
+    headers = {'User-Agent': 'linux:com.myapp:v.1'}
+
+    response = requests.get('https://www.reddit.com/r/{}/about'.
+                            format(subreddit), headers=headers)
+
+    if response.status_code == 404:
+        return 0
+
+    n_response = requests.get('https://www.reddit.com/r/{}/about.json'.
+                              format(subreddit), headers=headers).json()
+
+    return n_response.get('data').get('subscribers')
